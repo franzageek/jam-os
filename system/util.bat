efb72298dd7e2d0fbeb70aaeecd7c3eeb24c2dc4
FranzaGeek <88248950+franzageek@users.noreply.github.com>
2021-12-29 18:54:00 +0100

    Add files via upload

---
+++ system/util.bat
@@ -0,0 +1,15 @@
+@echo off
+mode 15,4
+cd ..
+cd boot
+rename boot.bat boot.jamx
+rename reset.bat reset.jamx
+rename shutdown.bat shutdown.jamx
+cd ..
+cd core
+rename snake.bat snake.jamapp
+rename system.bat system.jamapp
+cd ..
+cd system
+rename system.bat system.jamx
+exit
